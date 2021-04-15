36276f917588ca34e033308bbd4f21b40b2c6488
Chaithanya Kumar <chaithanyakumar.ds@gmail.com>
2021-04-15 15:12:21 +0530

    skills

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        if(this.scrollY > 20){
+        if(this.scrollY >= 0){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
-        if(this.scrollY > 500){
+        if(this.scrollY > 30){
             $('.scroll-up-btn').addClass("show");
         }else{
             $('.scroll-up-btn').removeClass("show");
